2b3d44d0e20adc69d9a78a05499eda2cda9f4d2e
Julia <Julia@Julias-MacBook-Pro.local>
2019-06-20 12:49:57 -0400

    Added Bootstrap and created responsive page layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import landing from "../styles/landing.scss";
 
 class App extends React.Component {
   render() {
-    return <div>Hello World.</div>;
+    return (
+      <Fragment>
+        <div className='container=fluid'>
+          <div className='row no-gutters'>
+            <div className='col-lg-6 col-md-6 col-sm'>Row 1 left side</div>
+            <div className='col-lg-6 col-md-6 col-sm'>Row 1 right side</div>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'>Hi row 2</div>
+          </div>
+        </div>
+        <div className='container-fluid'>
+          <div className='row no-gutters'>
+            <div className='col-lg-12'>Hi row 3</div>
+          </div>
+          <div className='row no-gutters'>
+            <div className='col-lg-7'>Hi row 4</div>
+          </div>
+          <div className='row no-gutters'>
+            <div className='col-lg-7'>Hi row 5</div>
+          </div>
+        </div>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-lg-12'>Hi row 6</div>
+          </div>
+          <div className='row'>
+            <div className='col-lg-6'>Row 7 left side</div>
+            <div className='col-lg-6'>Row 7 right side</div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
