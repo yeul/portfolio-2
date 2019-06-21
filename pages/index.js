43f79048ef1e2cb26381e8a47ab8727035049980
Julia <Julia@Julias-MacBook-Pro.local>
2019-06-21 14:02:57 -0400

    Added news and footer content

--- pages/index.js
+++ pages/index.js
@@ -40,22 +40,81 @@ class App extends React.Component {
         </div>
         <div className='news-section container-fluid'>
           <div className='row no-gutters'>
-            <div className='news-header col-lg-12'>News</div>
+            <div className='news-header col-lg-12'>
+              <div className='news-header-title'>News</div>
+            </div>
           </div>
           <div className='row no-gutters'>
-            <div className='news-stories col-lg-7'>Article section 1</div>
+            <div className='news-header col-lg-12'>
+              <div className='news-header-subtitle'>What We've Been Up To</div>
+            </div>
           </div>
           <div className='row no-gutters'>
-            <div className='news-stories col-lg-7'>Article section 2</div>
+            <div className='news-stories news-left col-lg-7'>
+              <img className='news-img-1' src='../static/images/unsplash-2.jpg' />
+              <div className='news-story-title'>Getting Settled - Saying 'hello!' to our new office!</div>
+            </div>
+            <div className='news-stories news-right col-lg-5'>
+              <img className='news-img-2' src='../static/images/unsplash-1.jpg' />
+              <div className='news-story-title'>Our Design Process - How we turn a concept into a reality</div>
+            </div>
+          </div>
+          <div className='row no-gutters'>
+            <div className='news-stories news-left col-lg-5'>
+              <img className='news-img-3' src='../static/images/unsplash-3.jpg' />
+              <div className='news-story-title'>Organizing code for scalability</div>
+            </div>
+            <div className='news-stories news-right col-lg-7'>
+              <img className='news-img-4' src='../static/images/unsplash-4.jpg' />
+              <div className='news-story-title'>SOURST turns 1! </div>
+            </div>
           </div>
         </div>
         <div className='footer container-fluid'>
-          <div className='row no-gutters'>
-            <div className='col-lg-12'>Footer logo</div>
+          <div className='footer-row row no-gutters'>
+            <div className='footer-logo col-lg-12'>SOURST</div>
           </div>
-          <div className='row'>
-            <div className='col-lg-6'>Contact info</div>
-            <div className='col-lg-6'>Address</div>
+          <div className='row no-gutters'>
+            <div className='contact-info col-lg-7'>
+              <div className='footer-nav'>
+                <ul>
+                  <li>Home</li>
+                  <li>About Us</li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            </div>
+            <div className='address-info col-lg-5'>
+              <div className='address'>
+                Toronto, ON
+                <br />
+                Canada M5V 2T6
+                <br />
+                301 Front St W
+              </div>
+            </div>
+          </div>
+          <div className='row no-gutters'>
+            <div className='footer-social-media col-lg-7'>
+              <ul>
+                <li>
+                  <i class='fab fa-twitter' />
+                </li>
+                <li>
+                  <i class='fab fa-facebook-f' />
+                </li>
+                <li>
+                  <i class='fab fa-linkedin-in' />
+                </li>
+              </ul>
+            </div>
+            <div className='footer-phone-email col-lg-5'>
+              <div className='phone-and-email'>
+                (416) 555 0195
+                <br />
+                sourst@sourst.com
+              </div>
+            </div>
           </div>
         </div>
       </Fragment>
